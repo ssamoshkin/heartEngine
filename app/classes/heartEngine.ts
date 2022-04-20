5c57f12aba738980308b1c0133ef9aae3eba7ca0
ssamoshkin <dedok1965@gmail.com>
2022-04-20 18:24:30 +0300

    app up

--- app/classes/heartEngine.ts
+++ app/classes/heartEngine.ts
@@ -1,17 +1,35 @@
 class HeartEngine {
-    private chambersNumber: number;
-    private readonly chambers: Chamber[];
-    private chambersIndexIds: Object;
-    private lastWorkingCamberId: number;
+    private chambersCount: number; // количество камер в сердце
+    private readonly chambers: Chamber[]; // храним массив объектов-камер
+    private chambersIndexIds: Object; // доступ к объектам-камер по индексам
+    private lastWorkingCamberId: number = 0; // последнеотработавшая камера
+    private controller = null;
+    private pulseCurrent = 0;
+    private pulseMax = 220;
+    private pulseMin = 20;
 
-    constructor(chambersNumber) {
-        this.chambersNumber = chambersNumber;
+    constructor(chambersCount) {
+        this.chambersCount = chambersCount;
         this.chambers = [];
-        for (let i = 0; i < chambersNumber; i += 1) {
-            const chamber = new Chamber();
+        // генерируем камеры в сердце без возможности управления их параметрами
+        for (let i = 0; i < chambersCount; i += 1) {
+            const chamber = new Chamber(this);
             this.chambers.push(chamber)
             this.addChamberIndex(chamber)
         }
+    }
+
+    start() {
+        this.controller = new NeuralController(this); // // управление камерами через отдельный контроллер
+        this.work();
+    }
+
+    stop() {
+        clearTimeout(this.controller.processId);
+    }
+
+    work() {
+        this.controller.tick();
     }
 
     addChamberIndex(chamber: Chamber): void {
@@ -19,37 +37,68 @@ class HeartEngine {
     }
 
     getLastWorkingChamber(): Chamber {
-        return null;
+        if (this.lastWorkingCamberId === 0) throw ('Сердце еще не было создано или запущено');
+        return this.getChamberById(this.lastWorkingCamberId);
     }
 
-    getChamber(n: number): Chamber {
-        return this.chambers[n];
+    getChambers(): Chamber[] {
+        return this.chambers;
     }
 
     getChamberById(id: number): Chamber {
         return this.chambersIndexIds[id];
     }
 
+    getActiveChamber(): Chamber {
+        return this.chambers[0];
+    }
+
+    getPulse(): number {
+        return this.pulseCurrent;
+    }
+
 }
 
 class Chamber {
-    private volume: number;
+    private volume: number = Math.floor(Math.random() * 100);
     private readonly id: number;
+    private stamina = 100;
+    protected name: string = 'Камера №';
 
-    constructor() {
+    constructor(heart: HeartEngine) {
         this.volume = 100;
-        this.id = 0;
+        this.id = heart.getChambers().length + 1;
+        this.name += this.id;
     }
 
     getId() {
         return this.id;
     }
+
+    push(pushPower: number = 100) {
+        this.stamina -= pushPower / 10;
+    }
 }
 
 class NeuralController {
+    // управляет камерами, балансирует нагрузку
     private heartEngine: HeartEngine;
+    private processId = null;
+
     constructor(HeartEngine) {
         this.heartEngine = HeartEngine;
+    }
+
+    tick() {
+        const chambers = this.heartEngine.getChambers();
+
+        // calculate next camber tick
+
+        this.processId = setTimeout(() => {
+            this.heartEngine.start();
+        }, this.heartEngine.getPulse());
+
+        return this.heartEngine.getActiveChamber();
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -2,6 +2,15 @@ const heartEngine = require('./classes/heartEngine')
 
 const heart = new heartEngine(4);
 
-setInterval(() => {
-    console.log(`последним отработал ${heart.getLastWorkingChamber().name}`)
+heart.start();
+
+let ticks = 100;
+const watcherId = setInterval(() => {
+    ticks -= 1;
+    if (ticks === 0) {
+        heart.stop();
+        clearInterval(watcherId);
+        console.log(`работа сердца остановлена`)
+    }
+    console.log(`последним отработала камера ${heart.getLastWorkingChamber()}`)
 }, 1000);
