8c3fb582b6ab7f76488f1dcf4948333c6d7c7f29
s.samoskin <s.samoskin@aoamt.ru>
2022-04-20 04:16:28 +0300

    dev init

--- app/classes/heartEngine.js
+++ app/classes/heartEngine.js
@@ -1,16 +1,25 @@
 class HeartEngine {
     constructor(cardiacNumber) {
         this.cardiacNumber = cardiacNumber;
+        this.cardiacs = [];
+        for (let i = 0; i < cardiacNumber; i += 1) {
+            this.cardiacs.push(new Cardiac())
+        }
     }
     getLastWorkingCardiac() {
         return null;
     }
+
 }
 
 class Cardiac {
     constructor() {
         this.volume = 100;
     }
+}
+
+class NeuralController {
+
 }
 
 module.exports = HeartEngine
