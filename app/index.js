41f046d86a1630d81c3359381139d3896a83504a
s.samoskin <s.samoskin@aoamt.ru>
2022-04-20 22:27:28 +0300

    clear old

--- app/index.js
+++ app/index.js
@@ -7,16 +7,7 @@ const heart = new HeartEngine(4);
 heart.pumpTo(brain);
 heart.start();
 
-let ticks = 100;
-
 const watcherId = setInterval(() => {
-    ticks -= 1;
-    if (ticks === 0) {
-        heart.stop();
-        clearInterval(watcherId);
-        console.log(`работа сердца остановлена`)
-    }
-
     const chamber = heart.getLastWorkingChamber();
 
     console.log(`Состояния: пульс - ${heart.getPulse()}, отработала камера ${chamber.getName()}, усталость ${chamber.getStamina()}`)
